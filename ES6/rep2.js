fd78f6e727b063066a0d2c313d009fa9ee556188
kezagiselle <kezagiselle776@gmail.com>
2025-02-25 13:53:45 +0200

    another one

--- ES6/rep2.js
+++ ES6/rep2.js
@@ -4,5 +4,9 @@ class human{
         this.age = age;
         this.height = height;
     }
-    
+    talk(language){
+        return this.name + (`can talk ${language}`);
+    }
 }
+const human1 = new human("keza", 24, 1.71);
+console.log(human1.talk("French"));
